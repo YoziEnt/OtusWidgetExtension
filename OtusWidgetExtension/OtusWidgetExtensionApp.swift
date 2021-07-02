b0a91c1462f7a0c5be10439a459181c5cca2dd1a
Oleg Mishkin <o.mishkin@squaregps.com>
2021-07-02 23:21:29 +0500

    Initial Commit

---
+++ OtusWidgetExtension/OtusWidgetExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtusWidgetExtensionApp.swift
+//  OtusWidgetExtension
+//
+//  Created by Олег Мишкин on 02.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct OtusWidgetExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
